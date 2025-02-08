a76e43ae191c8a93666190a4524dcfada91f0431
ikekra <chagankekra13@gmail.com>
2025-02-08 21:59:16 +0530

    Update CourseForm.tsx

--- src/components/CourseForm.tsx
+++ src/components/CourseForm.tsx
@@ -65,6 +65,7 @@ export function CourseForm({ onAddCourse }: CourseFormProps) {
           className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
         />
       </div>
+      
 
       <button
         type="submit"
